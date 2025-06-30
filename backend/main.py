471d9a969960e34833c41df9c1c35d49dc43ab2c
prashant1177 <prashantjpatil49@gmail.com>
2025-06-30 13:45:30 +0530

    url fixes

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
 jwt = JWTManager(app)
 
-CORS(app, supports_credentials=True, origins=["https://notes-bot-nine.vercel.app/"])  # Allow cross-origin requests from React
+CORS(app, supports_credentials=True, origins=["https://notes-bot-nine.vercel.app"])  # Allow cross-origin requests from React
 
 
 genaiClient = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
