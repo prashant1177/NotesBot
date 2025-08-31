e74578d46de454b74f7c8539357fb6743aa695aa
prashant1177 <prashantjpatil49@gmail.com>
2025-08-31 19:43:19 +0530

    Env errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,9 @@ function App() {
       setSidebarHide("w-64");
     }
   };
-
+  useEffect(() => {
+    console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+  }, []);
   return (
     <Router>
       <div>
