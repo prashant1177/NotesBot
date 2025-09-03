62a499caeec9c10e97bbf891273b191f2465ba97
prashant1177 <prashantjpatil49@gmail.com>
2025-09-03 15:41:51 +0530

    getting ready for production

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center space-x-2">
-            <span className="text-xl font-medium">Writingpapers</span>
+            <span className="text-xl font-medium">LaTeXWriter</span>
           </div>
           <div className="flex space-x-4">
             <ul className="text-lg flex flex-col items-center font-medium mt-4 rounded-lg bg-white/10 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
